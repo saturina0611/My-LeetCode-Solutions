5478c8aac5b1a8801bfe038360f81602e80d8047
Nguyễn Hồng Nam <nam.nh2516199@sis.hust.edu.vn>
2026-05-03 15:56:39 +0000

    Sync LeetCode submission - Monthly Transactions I (mysql)

---
+++ my-code/problems/monthly_transactions_i/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select
+    DATE_FORMAT(trans_date , '%Y-%m') month,
+    country,
+    count(*) trans_count,
+    count(if(state = 'approved' , 1 , null)) approved_count,
+    sum(amount) trans_total_amount,
+    sum(if(state = 'approved' , amount , 0)) approved_total_amount
+from Transactions
+group by 
+    month,
+    country;
