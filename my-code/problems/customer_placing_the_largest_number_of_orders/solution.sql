b569e040279768ae740abe599665e75f3f893442
Nguyễn Hồng Nam <nam.nh2516199@sis.hust.edu.vn>
2026-06-09 16:25:28 +0000

    Sync LeetCode submission - Customer Placing the Largest Number of Orders (mysql)

--- my-code/problems/customer_placing_the_largest_number_of_orders/solution.sql
+++ my-code/problems/customer_placing_the_largest_number_of_orders/solution.sql
@@ -1,8 +1,13 @@
-# Write your MySQL query statement below
-select 
-    customer_number
-from Orders
-group by
-    customer_number
-order by count(*) desc
-limit 0 , 1;
+select
+    o.customer_number
+from Orders o
+group by o.customer_number
+having o.customer_number = 
+        (select 
+            o2.customer_number
+        from Orders o2
+        group by o2.customer_number
+        order by count(o2.customer_number) desc
+        limit 1
+        );
+        
