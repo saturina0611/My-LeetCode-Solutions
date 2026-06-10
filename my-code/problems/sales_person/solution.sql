a193765f2c9470b09ca8e878809a714c04cd73d8
Nguyễn Hồng Nam <nam.nh2516199@sis.hust.edu.vn>
2026-06-10 16:26:51 +0000

    Sync LeetCode submission - Sales Person (mysql)

---
+++ my-code/problems/sales_person/solution.sql
@@ -0,0 +1,10 @@
+select 
+    s.name
+from Salesperson s
+where s.sales_id not in
+    (select
+        o.sales_id
+    from Orders o
+    left join Company c
+        on o.com_id = c.com_id
+    where c.name = 'RED');
